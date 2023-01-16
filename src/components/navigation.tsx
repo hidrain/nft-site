0b4651a501944bb53abe2267fdd95deb27bbb115
Yuliya Kurilchik <hidrainn@gmail.com>
2023-01-16 17:49:37 +0100

    Created an Accordion component for mobile menu

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { Button } from './button'
 import { Logo } from './logo'
-import { scrollTo } from '../helpers/scrollTo'
+import { useState } from 'react'
 
 const Section = styled.section`
     width: 100vw;
@@ -15,47 +15,152 @@ const NavBar = styled.nav`
     height: ${props => props.theme.navHeight};
     margin: 0 auto;
     background-color: ${props => props.theme.body};
+
+    .mobile {
+        display: none;
+    }
+
+    @media (max-width: 64em) {
+        .desktop {
+            display: none;
+        }
+        .mobile {
+            display: inline-block;
+        }
+    }
 `
-const Menu = styled.ul`
+const Menu = styled("ul") <{ click: boolean }>`
     display: flex;
     justify-content: space-between;
     align-items: center;
     list-style: none;
+
+    @media (max-width: 64em) {
+        /* 1024px */
+        position: fixed;
+        top: ${props => props.theme.navHeight};
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100vw;
+        height: ${props => `calc(100vh - ${props.theme.navHeight})`};
+        z-index: 50;
+        background-color: ${props => `rgba(${props.theme.bodyRgba}, 0.85)`};
+        backdrop-filter: blur(2px);
+
+        transform: ${props => props.click ? 'translateY(0)' : 'translateY(100%)'};
+        transition: all 0.3 easy;
+        flex-direction: column;
+        justify-content: center;
+    }
 `
 const MenuItem = styled.li`
-    margin: 0 1rem;
-    color: ${props => props.theme.text};
-    cursor: pointer;
+margin: 0 1rem;
+color: ${props => props.theme.text};
+cursor: pointer;
 
-    &::after { 
-        content: ' ';
-        display: block;
-        width: 0%;
-        height: 2px;
-        background: ${props => props.theme.text};
-        transition: width 0.3s ease;
-    }
-    &:hover::after { 
-        width: 100%;
-    }
+    &::after {
+    content: ' ';
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${props => props.theme.text};
+    transition: width 0.3s ease;
+}
+    &:hover::after {
+    width: 100%;
+}
+@media (max-width: 64em) {
+        /* 1024px */
+        margin: 1rem auto;
+
+        &::after {
+            display: none;
+}
+}
+
+`
+const HamburgerMenu = styled("span") <{ click: boolean }>`
+width: ${props => props.click ? '2rem' : '1.5rem'};
+height: 2px;
+background: ${props => props.theme.text};
+position: absolute;
+top: 2rem;
+left: 50%;
+transform: ${props => props.click ? 'translateX(-50%) rotate(90deg)' : 'translateX(-50%) rotate(0)'};
+display: none;
+justify-content: center;
+align-items: center;
+cursor: pointer;
+transition: all 0.3s ease;
+
+@media(max-width: 64em) {
+    /* 1024px */
+    display: flex;
+}
+
+    &::after, &::before {
+    content: ' ';
+    width: ${props => props.click ? '1rem' : '1.5rem'};
+    height: 2px;
+    right: ${props => props.click ? '-2px' : '0'};
+    background: ${props => props.theme.text};
+    transition: all 0.3s ease;
+    position: absolute;
+}
+    &::after {
+    top: ${props => props.click ? '0.3rem' : '0.5rem'};
+    transform: ${props => props.click ? 'rotate(-40deg)' : 'rotate(0)'};
+}
+    &::before {
+    bottom: ${props => props.click ? '0.3rem' : '0.5rem'};
+    transform: ${props => props.click ? 'rotate(40deg)' : 'rotate(0)'};
+}
 `
 
+
 export const Navigation = () => {
+
+    const [click, setClick] = useState(false)
+
+    const scrollTo = (id: string) => {
+
+        let element = document.getElementById(id);
+        element?.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'nearest'
+        })
+        setClick(!click)
+    }
+
     return (
         <Section id='navigation'>
             <NavBar>
                 <Logo />
-                <Menu>
+                <HamburgerMenu
+                    click={click}
+                    onClick={() => setClick(!click)} >
+                    &nbsp;
+                </HamburgerMenu>
+                <Menu click={click}>
                     <MenuItem onClick={() => scrollTo('home')}>Home</MenuItem>
                     <MenuItem onClick={() => scrollTo('about')}>About</MenuItem>
                     <MenuItem onClick={() => scrollTo('roadmap')}>RoadMap</MenuItem>
                     <MenuItem onClick={() => scrollTo('showcase')}>ShowCase</MenuItem>
                     <MenuItem onClick={() => scrollTo('team')}>Team</MenuItem>
                     <MenuItem onClick={() => scrollTo('faq')}>FAQ</MenuItem>
+                    <MenuItem>
+                        <div className="mobile">
+                            <Button text='Connect Wallet' link='#' />
+                        </div>
+                    </MenuItem>
                 </Menu>
-                <Button text='Connect Wallet' link='#' />
+                <div className="desktop">
+                    <Button text='Connect Wallet' link='#' />
+                </div>
             </NavBar>
-        </Section>
+        </Section >
 
     )
 }
--- src/helpers/scrollTo.tsx
+++ src/helpers/scrollTo.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 export const scrollTo = (id: string) => {
+
+    const [click, setClick] = useState(false)
+
     let element = document.getElementById(id);
     element?.scrollIntoView({
         behavior: 'smooth',
         block: 'start',
         inline: 'nearest'
     })
+
+    setClick(!click)
 
 }
